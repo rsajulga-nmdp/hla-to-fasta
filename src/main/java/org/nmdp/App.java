3fbf63bb3bdd29679aac9da5243e7a0e46a8ed38
rsajulga-nmdp <rsajulga@nmdp.org>
2020-11-11 13:04:25 -0600

    add try catch system to catch errors

--- src/main/java/org/nmdp/App.java
+++ src/main/java/org/nmdp/App.java
@@ -14,42 +14,45 @@ public class App {
         File input = new File(inputFile);
         String fileName = input.getName().substring(0, input.getName().lastIndexOf("."));
         Scanner sc = new Scanner(new File(inputFile));
-
-
-
-        while(sc.hasNext()){
-            String[] data = sc.nextLine().split(",");
-//            String folderName = data[0];
-            String projectName = data[1];
-            String sampleID = data[2];
-            String alleleID = data[3];
-            String alleleName = data[4];
-//            String glString = data[5];
-            
-            if (data.length > 3){
-                StringBuilder sb = new StringBuilder();
-                sb.append(">");
-                sb.append(sampleID);
-                sb.append(DIVIDER);
-                sb.append(projectName);
-                sb.append(DIVIDER);
-                sb.append(alleleID);
-                sb.append(DIVIDER);
-                sb.append(alleleName);
-                sb.append("\n");
-                for(int i = 6; i < data.length; i++){
-                    sb.append(data[i]);
-                }
-                if (alleleName.indexOf('-') >= 0){
-                    String locus = getKey(alleleName);
-                    if (locus.length() > 0){
-                        if(!printMap.containsKey(locus)){
-                            printMap.put(locus, new PrintWriter(fileName + "_"+ locus + ".fasta"));
-                        }
-                        printMap.get(locus).println(sb.toString());
-                    }
-                }
-            }
+        
+        for(int lineNum=1; sc.hasNext(); lineNum++){
+        	try {
+	            String[] data = sc.nextLine().split(",");
+	//            String folderName = data[0];
+	            String projectName = data[1];
+	            String sampleID = data[2];
+	            String alleleID = data[3];
+	            String alleleName = data[4];
+	//            String glString = data[5];
+	            
+	            if (data.length > 3){
+	                StringBuilder sb = new StringBuilder();
+	                sb.append(">");
+	                sb.append(sampleID);
+	                sb.append(DIVIDER);
+	                sb.append(projectName);
+	                sb.append(DIVIDER);
+	                sb.append(alleleID);
+	                sb.append(DIVIDER);
+	                sb.append(alleleName);
+	                sb.append("\n");
+	                for(int i = 6; i < data.length; i++){
+	                    sb.append(data[i]);
+	                }
+	                if (alleleName.indexOf('-') >= 0){
+	                    String locus = getKey(alleleName);
+	                    if (locus.length() > 0){
+	                        if(!printMap.containsKey(locus)){
+	                            printMap.put(locus, new PrintWriter(fileName + "_"+ locus + ".fasta"));
+	                        }
+	                        printMap.get(locus).println(sb.toString());
+	                    }
+	                }
+	            }
+        	} catch (Exception e) {
+        		System.out.println("Something went wrong at line number " + lineNum);
+        		System.out.println(e);
+        	}
         }
 
         for(PrintWriter pw : printMap.values()){
